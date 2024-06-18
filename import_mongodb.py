276dae45ef13a9692244d66636a3ae03fb8105eb
thiagonoq <thiago_neves@hotmail.com>
2024-06-18 16:00:09 -0300

    ✨ Now, import jsons saves them on Drive

--- import_mongodb.py
+++ import_mongodb.py
@@ -5,6 +5,7 @@ import config
 import json
 import conserta_json
 import atualiza_devs
+import google_drive_api
 from pathlib import Path
 
 abs_path = Path(__file__).parent.parent
@@ -20,24 +21,28 @@ collections = ['clients','cta_list_response', 'analytics']
 json_path = abs_path / analytics_file
 
 def import_json(client, db, collections):
+    jsons_list = []
     for collection in collections:
         # if collection == 'clients':
         #     atualiza_devs.update_dev_info(db)
-        
+
         date_name = 'created_at' if collection != 'cta_list_response' else 'date'
         date = datetime.combine(datetime.now(), datetime.min.time())
 
         document = db[collection].find({date_name: {"$lte": date}})
-
-        with open(f'{abs_path}\\videoai.{collection}.json', 'w', encoding='utf-8') as file:
+        data_path = abs_path / f'videoai.{collection}.json'
+        
+        with open(data_path, 'w', encoding='utf-8') as file:
             json.dump(list(document), file, default=str, ensure_ascii=False)
+
+        jsons_list.append(data_path)
 
         if collection == 'analytics':
             empty_entries = conserta_json.data_handling()
 
             if empty_entries:
                 del_entries_db(db, collection, date)
-
+    upload_to_google_drive(jsons_list)
     print('Importação concluida')
     client.close()
 
@@ -93,6 +98,37 @@ def atualiza_produtos(db):
     
     print('Produtos atualizados')
     
+def upload_to_google_drive(jsons_list):
+    drive = google_drive_api.auth_drive()
+    google_json_folder_id = '1WUNM-yF2zgDwDCKe-qX5Wl8lI-cC0A71'
+    
+    for json_path in jsons_list:
+        try:
+            file_name = json_path.name
+
+            files_list = drive.ListFile({
+                'q': f"title = '{file_name}' and '{google_json_folder_id}' in parents and trashed=false"
+            }).GetList()
+
+            if files_list:
+                for file in files_list:
+                    print(f"Deleting {file['title']}...")
+                    file.Delete()
+
+            file_drive = drive.CreateFile({
+                'title': file_name,
+                'parents': [{'id': google_json_folder_id}]})
+
+            file_drive.SetContentFile(json_path)
+
+            file_drive.Upload()
+
+            print(f'Arquivo {file_name} enviado com sucesso!')
+
+        except Exception as e:
+            print(e)
+            continue
+
 if __name__ == '__main__':
     import_json(client, db, collections)
     # export_json_to_mongodb(client, db, json_path)
